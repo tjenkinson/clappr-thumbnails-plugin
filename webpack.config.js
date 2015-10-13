f6fe2d1f4d7e709007fa1c8730546ec41ca51f8b
Tom Jenkinson <tjcode@outlook.com>
2015-10-13 15:46:54 +0100

    Hello world!

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+var Clean = require('clean-webpack-plugin');
+
+var plugins = [
+  new Clean(['dist'])
+];
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  plugins: plugins,
+  externals: {
+    clappr: 'Clappr',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+            compact: true,
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'clappr-thumbnails-plugin.js',
+    library: 'ClapprThumbnailsPlugin',
+    libraryTarget: 'umd',
+  },
+};
